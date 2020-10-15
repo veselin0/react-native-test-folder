9358a9901983ea1f9b52a46245fbddee942a280d
Veselin <vesselin60@gmail.com>
2020-10-15 10:52:50 +0300

    Switch

--- rnCpsTest/App.js
+++ rnCpsTest/App.js
@@ -1,38 +1,25 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import {StyleSheet, View, Text, Button, Alert} from 'react-native';
+import React, {useState} from 'react';
+import {View, Switch, StyleSheet} from 'react-native';
 
 export default function App() {
-  const onPressHandler = () => {
-    Alert.alert('button pressed');
-  };
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
 
   return (
-    <View style={styles.screen}>
-      <Text>Gocho</Text>
-      <Button
-        onPress={onPressHandler}
-        title="Learn More"
-        color="#841584"
-        accessibilityLabel="Learn More about this purple button"
+    <View style={styles.container}>
+      <Switch
+        trackColor={{false: '#767577', true: '#81b0ff'}}
+        thumbColor={isEnabled ? 'green' : 'red'}
+        onValueChange={toggleSwitch}
+        value={isEnabled}
       />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  screen: {
-    margin: '5%',
-    width: '90%',
-    height: '90%',
-    backgroundColor: 'dodgerblue',
+  container: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
